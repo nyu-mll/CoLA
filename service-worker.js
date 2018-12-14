75048e62e8f7b083dd1f8ad65cfb677de1c9e26a
Amanpreet Singh <asg@fb.com>
2018-12-14 10:56:53 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","d1f419e9b333c60ec6cca2f171b113de"],["static/css/main.823ef301.css","7489dc48e49ed11f8aed80263c0500a1"],["static/js/main.69d52bbc.js","7f1bac660e281b38c0c6c36565fbb401"]];
+var precacheConfig = [["index.html","6f8b775d045c82c408eae661039ad6f7"],["static/css/main.823ef301.css","7489dc48e49ed11f8aed80263c0500a1"],["static/js/main.b5c72be0.js","893e027d984aa5b525f3987b00b7d960"]];
 var cacheName = 'sw-precache-v3-my-magical-cache-machine-' + (self.registration ? self.registration.scope : '');
 
 
